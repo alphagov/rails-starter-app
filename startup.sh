6b04e60172cee83f016a1f257106fce1a45c55b3
George Lund <george.lund@digital.cabinet-office.gov.uk>
2019-05-08 21:31:56 +0100

    Remove pid files if we've restarted the Docker container

--- startup.sh
+++ startup.sh
@@ -5,4 +5,5 @@ THIS_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 THIS_DIR=`basename $THIS_PATH`
 DOCKER_IMAGE_NAME="${THIS_DIR}-dev"
 
+rm -f $THIS_PATH/tmp/pids/* 
 docker run -it -p 3000:3000 --rm -v $THIS_PATH:/usr/src -w /usr/src $DOCKER_IMAGE_NAME rails s -b 0.0.0.0

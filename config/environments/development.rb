60df729fffa6808cebc9111d9a45a8d13ceebd3d
George Lund <george.lund@digital.cabinet-office.gov.uk>
2019-05-09 10:48:48 +0100

    Eliminate annoying error message regarding whitelist IP on Docker
     - https://stackoverflow.com/a/55716426/297790

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,4 +58,14 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # https://stackoverflow.com/a/20012536/4862360
+  if File.read('/proc/1/cgroup').include?('docker')
+    # https://stackoverflow.com/a/24716645/4862360
+    host_ip = `/sbin/ip route|awk '/default/ { print $3 }'`.strip
+
+    BetterErrors::Middleware.allow_ip!(host_ip) if defined?(BetterErrors::Middleware)
+    config.web_console.whitelisted_ips << host_ip
+  end
+
 end
